2a6a36af223859a7fa8da59efbe41cab475769a8
Behrouz Safari <behrouz.safari@gmail.com>
2023-07-21 00:03:50 +0200

    simple nemueric finished

--- Simple/funcs.py
+++ Simple/funcs.py
@@ -5,67 +5,27 @@ from astropy import units as u
 from astropy.units.quantity import Quantity as Q
 from scipy.integrate import odeint
 
-def where(th):
-    q1 = 0*u.rad           <= th < (np.pi/2)*u.rad
-    q2 = (np.pi/2)*u.rad   <= th < np.pi*u.rad
-    q3 = np.pi*u.rad       <= th < (3*np.pi/2)*u.rad
-    q4 = (3*np.pi/2)*u.rad <= th < 2*np.pi2*u.rad
-    return q1, q2, q3, q4
 
+def simple_pend_num(l, th0, vx0=0, vy0=0, dt=0.01, g=9.81, steps=1000):
+    th, vx, vy = th0*(np.pi/180), vx0, vy0
 
-def simple_pend_num(l, th0=45, vx0=0, vy0=0, dt=0.01, g=9.81, steps=1000):
-    if not isinstance(l, Q):
-        l = l * u.m
-    if not isinstance(th0, Q):
-        th0 = (th0 * u.deg).to('rad')
-    if not isinstance(vx0, Q):
-        vx0 = vx0 * u.Unit('m/s')
-    if not isinstance(vy0, Q):
-        vy0 = vy0 * u.Unit('m/s')
-    if not isinstance(dt, Q):
-        dt = dt * u.s
-    if not isinstance(g, Q):
-        g = g * u.Unit('m/s2')
-
-    th, vx, vy = th0, vx0, vy0
-
-    q1, q2, q3, q4 = where(th)
-
-    if q1:
-        x = l * np.sin(th)
-        y = l * np.cos(th)
-    if q2:
-        x = l * np.cos(th-(np.pi/2)*u.rad)
-        y = -l * np.sin(th-(np.pi/2)*u.rad)
-    else:
-        print('*'*70)
-
-
+    x = l * np.sin(th)
+    y = l * np.cos(th)
+    
     m = 1
-
     xx = [x]
     yy = [y]
 
-    for i in range(steps):    
-        print(th.to('deg'), '=>', x)
-        if q1:
-            th = np.arcsin(x/l)
-        elif q2:
-            th = np.arccos(x/l)+(np.pi/2)*u.rad
+    for i in range(steps):
+        th = np.arcsin(x/l)
         Fx = -m*g*np.sin(th)*np.cos(th)
         vx = vx + (Fx/m)*dt
         x = x + vx*dt
-        y = np.sqrt(l**2-x**2) if x<l else 0
-        if q2:
-            y = -y
-        
+        y = np.sqrt(l**2-x**2)
         xx.append(x)
         yy.append(y)
-        q1, q2, q3, q4 = where(th)
 
-    xx = np.array([i.value for i in xx]) * xx[0].unit
-    yy = np.array([i.value for i in yy]) * yy[0].unit
-    return xx, yy
+    return np.array(xx), np.array(yy)
 
 
 def simple_pend_anal(l, th0, thp0=0, dt=0.01, g=9.81, steps=5000):
@@ -85,25 +45,6 @@ def simple_pend_anal(l, th0, thp0=0, dt=0.01, g=9.81, steps=5000):
     return xx, yy
 
 
-def set_lim(ax, xx, yy):
-    minxs = min([xx.min(), 0])
-    maxxs = max([xx.max(), 0])
-    minys = min([yy.min(), 0])
-    maxys = max([yy.max(), 0])
-
-    minxs = minxs*1.2 if minxs<0 else minxs*0.8
-    minys = minys*1.2 if minys<0 else minys*0.8
-    maxxs = maxxs*0.8 if maxxs<0 else maxxs*1.2
-    maxys = maxys*0.8 if maxys<0 else maxys*1.2
-
-    if maxys==0:
-        maxys=0.5
-    
-    ax.set_xlim([minxs, maxxs])
-    ax.set_ylim([minys, maxys])
-    
-    return ax
-
 
 def animate(xx, yy, interval=20):
     fig = plt.figure()
@@ -116,8 +57,6 @@ def animate(xx, yy, interval=20):
 
     radius = 0.05 * r
     cir = ax.add_patch(plt.Circle((x0, y0), radius, fc='r', zorder=3))
-
-    #ax = set_lim(ax, xx, yy)
     
     ax.set_xlim([-1.2*r, 1.2*r])
     ax.set_ylim([-1.2*r, 1.2*r])
